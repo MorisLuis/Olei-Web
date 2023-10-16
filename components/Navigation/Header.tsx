2d6a13639a6848d104791c78fb6dcb0789731c46
MorisLuis <73044817+MorisLuis@users.noreply.github.com>
2023-10-15 18:39:01 -0600

    S / Header UI

--- components/Navigation/Header.tsx
+++ components/Navigation/Header.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from 'react';
 import styles from "./../../styles/Navigation/Header.module.scss";
 
 import { api } from '@/api/api';
-import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import { AuthContext, CartContext, ClientContext, FiltersContext } from '@/context';
 import { ModalSearch } from '../Modals/ModalSearch';
@@ -21,7 +20,7 @@ const Header = ({
     setOpenModalMenu
 }: Props) => {
 
-    const { replace, push, pathname } = useRouter()
+    const { push, pathname } = useRouter()
     const { user } = useContext(AuthContext);
     const { numberOfItems } = useContext(CartContext);
     const { client, selectClient, setClientChanged } = useContext(ClientContext);
@@ -29,16 +28,6 @@ const Header = ({
 
     const [modalSearchVisible, setModalSearchVisible] = useState(false);
     const [modalClientsVisible, setModalClientsVisible] = useState(false)
-
-    const onLogOut = async () => {
-        try {
-            await api.post('/api/auth/logout');
-            Cookies.remove("token")
-            replace("/login")
-        } catch (error) {
-            console.log({ error })
-        }
-    }
 
     // Search
 
@@ -193,12 +182,12 @@ const Header = ({
                             <></>
                             :
                             <div className={`${styles.right} display-flex`}>
-                                <div className={`${styles.orders} display-flex align cursor`} onClick={() => push("/profile/request")}>
+                                {/* <div className={`${styles.orders} display-flex align cursor`} onClick={() => push("/profile/request")}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6 icon">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                                     </svg>
                                     <p>Pedidos</p>
-                                </div>
+                                </div> */}
 
                                 <div className={`${styles.profile} display-flex allCenter`} onClick={() => setOpenModalMenu?.(true)}>
                                     <div className={styles.info}>
