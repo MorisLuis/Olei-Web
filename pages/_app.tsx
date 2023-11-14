28533cdd3375364fc4fafd75fc84182940713f7e
MorisLuis <73044817+MorisLuis@users.noreply.github.com>
2023-11-13 20:43:21 -0600

    NF / NextNProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/Globals.scss';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
 
+  const router = useRouter()
+
   return (
     <>
+      {
+        router.route !== '/products' &&
+        <NextNProgress color="#EDBD42" height={4} />
+      }
       <AuthProvider>
         <CartProvider>
           <ClientProvider>
