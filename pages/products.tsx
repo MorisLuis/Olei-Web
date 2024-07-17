1fc5e781f817e7fda02cf9d2dacf61cfc3d32ebc
“MorisLuis” <moradoluisenrique@gmail.com>
2024-07-17 09:41:38 -0600

    setup

--- pages/products.tsx
+++ pages/products.tsx
@@ -187,7 +187,7 @@ export default function Home({ productsProps }: Props) {
 
   useEffect(() => {
     const getProducts = async () => {
-      let url = `/api/product?page=1&limit=20`;
+      let url = `api/product?page=1&limit=20`;
       const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}${url}`);
       console.log({data})
     } 
