2793188ced7617120b296ae9edd70f48dbc9f039
“MorisLuis” <moradoluisenrique@gmail.com>
2024-09-14 14:53:01 -0600

    setup

--- hooks/useErrorHandler.tsx
+++ hooks/useErrorHandler.tsx
@@ -1,34 +1,38 @@
 import { useRouter } from 'next/router';
 import toast from 'react-hot-toast';
 
-export const useErrorHandler = () =>  {
+export const useErrorHandler = () => {
     const router = useRouter();
 
     const handleError = (error: any) => {
-
-        const { response: { status } } = error ?? {}
+        // Verifica que error y error.response existan antes de acceder a error.response.status
+        const status = error?.response?.status;
 
         if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'local') return;
 
-        if( status ) {
-            if (status === 404) {
-                router.push('/404');
-            } else if (status === 401) {
-                router.push('/login');
-            } else if (status === 403) {
-                router.push('/login');
-            } else if (status === 500) {
-                router.push('/500');
-            } else {
-                router.push('/400');
+        if (status) {
+            switch (status) {
+                case 404:
+                    router.push('/404');
+                    break;
+                case 401:
+                case 403:
+                    router.push('/login');
+                    break;
+                case 500:
+                    router.push('/500');
+                    break;
+                default:
+                    router.push('/404');
+                    break;
             }
         } else {
-            return toast.error("Algo salio mal!")
+            toast.error("Algo salió mal!");
         }
-
     };
 
     return { handleError };
 }
+
 
 export default useErrorHandler;
