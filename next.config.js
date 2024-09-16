8fec5cd2fe3b9a5476629c08433b4f2dc831ba2e
“MorisLuis” <moradoluisenrique@gmail.com>
2024-09-16 17:45:39 -0600

    setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
-// next.config.js
-
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['oleistorage.blob.core.windows.net', 'm.media-amazon.com'],
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Aplica a todas las rutas
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.oleionline.com/', // Cambia por tu dominio de producción
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true', // Permite enviar cookies con solicitudes cross-origin
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS', // Métodos permitidos
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Accept, Content-Type, Authorization', // Cabeceras permitidas
+          },
+        ],
+      },
+    ];
+  },
 };
