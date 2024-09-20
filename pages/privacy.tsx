fee7e39cce10cf52928ee138ae7a4a6971cc6c69
“MorisLuis” <moradoluisenrique@gmail.com>
2024-09-20 00:21:46 -0600

    setup

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -7,31 +7,16 @@ const Privacy = () => {
 
     return (
         <Layout>
-            <div
-                style={{
-                    padding: "100px 200px"
-                }}
-            >
-                <h2
-                    style={{
-                        marginBottom: "10px"
-                    }}
-                >Última modificación: {privacyPolicy.lastModified}</h2>
-                <p
-                    style={{
-                        marginBottom: "20px"
-                    }}
-                >
+            <div style={{ padding: "100px 200px" }}>
+                <h2 style={{ marginBottom: "10px" }} >
+                    Última modificación: {privacyPolicy.lastModified}
+                </h2>
+                <p style={{ marginBottom: "20px" }} >
                     {privacyPolicy.privacyPolicy}
                 </p>
                 <ol>
                     {privacyPolicy.points.map((point, index) => (
-                        <li
-                            key={index}
-                            style={{
-                                marginBottom: "10px"
-                            }}
-                        >{point}</li>
+                        <li key={index} style={{ marginBottom: "10px" }}>{point}</li>
                     ))}
                 </ol>
             </div>
