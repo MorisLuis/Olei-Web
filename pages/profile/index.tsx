73a6af08f05130f30430b6af07794cffb2b0c5cf
“MorisLuis” <moradoluisenrique@gmail.com>
2024-11-10 02:31:20 -0600

    F / min width global

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -18,27 +18,3 @@ const Profile = () => {
 }
 
 export default Profile
-
-
-/* 
-                <div className={styles.account}>
-                <section className={styles.info}>
-                    <div className={styles.form}>
-                        <div className={styles.item}>
-                            <label htmlFor="Cuenta">Cuenta</label>
-                            <p>{user?.Id_UsuarioOOL}</p>
-                        </div>
-
-                        <div className={styles.item}>
-                            <label htmlFor="Nombre">Nombre</label>
-                            <p>{user?.Nombre}</p>
-                        </div>
-
-                        <div className={styles.item}>
-                            <label htmlFor="Cliente">Tipo de Cliente</label>
-                            <p>{user?.TipoUsuario === 1 ? "Cliente" : "Vendedor"}</p>
-                        </div>
-                    </div>
-                </section>
-            </div >
-*/
